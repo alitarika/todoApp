ac7957cbf0c3d045501e9ee3b9b748ec18c317b7
alitarika <alitarikalacam@gmail.com>
2024-07-27 21:00:39 +0300

    Layout of the todoapp is established

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="flex justify-between w-[972px] items-center text-[11px] mt-[12px] opacity-30">
+      <small className="text-[11px]">
+        &copy; 2024. Copyright by alitarika.
+      </small>
+      <p>
+        Version <b>1</b>
+      </p>
+    </footer>
+  );
+}
