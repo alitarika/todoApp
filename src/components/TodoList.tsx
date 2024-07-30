896f2d6a97b85b79fa8cfd529801edc533e96a67
alitarika <alitarikalacam@gmail.com>
2024-07-30 12:26:35 +0300

    todolist component implemented

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,3 +1,19 @@
+const initialTodos = ["buy groceries", "walk the dog", "do the laundry"];
+
 export default function TodoList() {
-  return <ul></ul>;
+  return (
+    <ul>
+      {initialTodos.map((todo) => (
+        <li
+          key={todo} // would have been better if we have had id in our list and we had objects inside the list
+          // because todolist would not be more than 100 most likely. It is okay to use same keys. React can handle it with performance.
+
+          className="flex justify-between items-center px-8 h-[50px] cursor-pointer text-[14px] border-b border-black/[8%]"
+        >
+          <span>{todo}</span>
+          <button>❌</button>
+        </li>
+      ))}
+    </ul>
+  );
 }
