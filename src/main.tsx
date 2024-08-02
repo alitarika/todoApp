32e1f6798304735f39013dd1282d213e68053ab7
alitarika <alitarikalacam@gmail.com>
2024-08-02 11:11:47 +0300

    kinde provider for development environment (localhost) implemented

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App.tsx";
 import "./index.css";
+import TodosContextProvider from "./contexts/TodosContextProvider.tsx";
+import { KindeProvider } from "@kinde-oss/kinde-auth-react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <KindeProvider
+      clientId="447e2400c05c4dba9b90a3e54e6dfbc3"
+      domain="https://alitarikalacam.kinde.com"
+      redirectUri="http://localhost:5173"
+      logoutUri="http://localhost:5173"
+    >
+      <TodosContextProvider>
+        <App />
+      </TodosContextProvider>
+    </KindeProvider>
   </React.StrictMode>
 );
